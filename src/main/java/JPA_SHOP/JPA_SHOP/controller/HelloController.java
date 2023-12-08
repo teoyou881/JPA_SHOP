ee84a68c11b61f9f921b69ead682df392c7ee9d5
Hyungchul You <teoyou881@gmail.com>
2023-12-08 14:07:16 -0500

    feat(Member): create Member controller, entity, repository

---
+++ src/main/java/JPA_SHOP/JPA_SHOP/controller/HelloController.java
@@ -0,0 +1,15 @@
+package JPA_SHOP.JPA_SHOP.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "Wellcome!");
+    return "hello";
+  }
+}
---
+++ src/main/java/JPA_SHOP/JPA_SHOP/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package JPA_SHOP.JPA_SHOP.repository;
+
+import JPA_SHOP.JPA_SHOP.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  public Long save(Member member) {
+    em.persist(member);
+    // Separate commands and queries.
+    return member.getId();
+  }
+
+  public Member find(Long id) {
+    return em.find(Member.class, id);
+  }
+}
