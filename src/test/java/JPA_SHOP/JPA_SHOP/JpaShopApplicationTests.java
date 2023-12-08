24ca9254df55ea5db259a014e2fe63983a183be2
Hyungchul You <teoyou881@gmail.com>
2023-12-08 13:05:56 -0500

    Initial commit

---
+++ src/test/java/JPA_SHOP/JPA_SHOP/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package JPA_SHOP.JPA_SHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
