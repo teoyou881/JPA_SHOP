5ecea9df8761212e24b8a15a57288850bff97342
Hyungchul You <teoyou881@gmail.com>
2023-12-10 00:17:55 -0500

    feat(ItemService): create ItemService

---
+++ src/main/java/JPA_SHOP/JPA_SHOP/service/ItemService.java
@@ -0,0 +1,29 @@
+package JPA_SHOP.JPA_SHOP.service;
+
+import JPA_SHOP.JPA_SHOP.domain.item.Item;
+import JPA_SHOP.JPA_SHOP.repository.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+  private final ItemRepository itemRepository;
+
+  @Transactional
+  public void saveItem(Item item) {
+    itemRepository.save(item);
+  }
+
+  public Item findOne(Long itemId) {
+    return itemRepository.findOne(itemId);
+  }
+
+  public List<Item> findItems() {
+    return itemRepository.findAll();
+  }
+}
