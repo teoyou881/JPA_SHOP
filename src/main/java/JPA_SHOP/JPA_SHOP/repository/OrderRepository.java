d492cb14455fad5361d3dca6d30b8f02ca25d89b
Hyungchul You <teoyou881@gmail.com>
2023-12-10 16:22:21 -0500

    feat(OrderRepository): use JPA criteria for dynamic para

--- src/main/java/JPA_SHOP/JPA_SHOP/repository/OrderRepository.java
+++ src/main/java/JPA_SHOP/JPA_SHOP/repository/OrderRepository.java
@@ -2,6 +2,15 @@ package JPA_SHOP.JPA_SHOP.repository;
 
 import JPA_SHOP.JPA_SHOP.domain.Order;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +26,33 @@ public class OrderRepository {
 
   public Order findOne(Long id) {
     return em.find(Order.class, id);
+  }
+
+
+  /*
+   * JPA Criteria
+   * */
+  public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+    Root<Order> o = cq.from(Order.class);
+    Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+    ArrayList<Predicate> criteria = new ArrayList<>();
+
+    if (orderSearch.getOrderStatus() != null) {
+      Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+      criteria.add(status);
+    }
+    if (orderSearch.getMemberName() != null) {
+      Predicate name = cb.like(m.get("name"), "%" + orderSearch.getMemberName() + "%");
+      criteria.add(name);
+    }
+
+    cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+    TypedQuery<Order> query = em.createQuery(cq).setMaxResults(100);
+    return query.getResultList();
+
   }
 }
---
+++ src/main/java/JPA_SHOP/JPA_SHOP/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package JPA_SHOP.JPA_SHOP.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+  private String memberName;
+  private String orderStatus;
+}
