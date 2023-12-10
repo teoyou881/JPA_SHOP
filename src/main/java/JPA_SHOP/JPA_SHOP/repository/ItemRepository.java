0eaa437b4785f6cd9f6aa73ebd16550a8ce77f17
Hyungchul You <teoyou881@gmail.com>
2023-12-10 00:16:01 -0500

    feat(ItemRepository): create ItemRepository

---
+++ src/main/java/JPA_SHOP/JPA_SHOP/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package JPA_SHOP.JPA_SHOP.repository;
+
+import JPA_SHOP.JPA_SHOP.domain.item.Item;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+  private final EntityManager em;
+
+  public void save(Item item) {
+    if (item.getId() == null) {
+      // new item
+      em.persist(item);
+    } else {
+      // update item
+      em.merge(item);
+    }
+  }
+
+  public Item findOne(Long id) {
+    return em.find(Item.class, id);
+  }
+
+  public List<Item> findAll() {
+    return em.createQuery("select i from Item i", Item.class)
+        .getResultList();
+  }
+}
