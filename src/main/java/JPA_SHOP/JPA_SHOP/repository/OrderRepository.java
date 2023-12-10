019eeb5af13dd3cb6baaff3278a3c3eaf210c29a
Hyungchul You <teoyou881@gmail.com>
2023-12-10 03:54:54 -0500

    feat(OrderRepository): create OrderRepository

---
+++ src/main/java/JPA_SHOP/JPA_SHOP/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package JPA_SHOP.JPA_SHOP.repository;
+
+import JPA_SHOP.JPA_SHOP.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+  private final EntityManager em;
+
+  public void save(Order order) {
+    em.persist(order);
+  }
+
+  public Order findOne(Long id) {
+    return em.find(Order.class, id);
+  }
+}
