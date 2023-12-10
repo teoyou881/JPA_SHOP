27a1dfc519693cd3da4ffc29b371ca8a42262875
Hyungchul You <teoyou881@gmail.com>
2023-12-09 22:53:06 -0500

    feat(Category/domain): create relational convenience method

--- src/main/java/JPA_SHOP/JPA_SHOP/Domain/Category.java
+++ src/main/java/JPA_SHOP/JPA_SHOP/Domain/Category.java
@@ -3,6 +3,7 @@ package JPA_SHOP.JPA_SHOP.Domain;
 import JPA_SHOP.JPA_SHOP.Domain.item.Item;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -36,10 +37,16 @@ public class Category {
       inverseJoinColumns = @JoinColumn(name = "item_id"))
   private List<Item> items = new ArrayList<>();
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "parent_id")
   private Category parent;
 
   @OneToMany(mappedBy = "parent")
   private List<Category> child = new ArrayList<>();
+
+  // === Relational convenience method === //
+  public void addChildCategory(Category child) {
+    this.child.add(child);
+    child.setParent(this);
+  }
 }
