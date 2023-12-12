ff1ecfdbd653e15761a36b71d8b328aecc5fee3d
Hyungchul You <teoyou881@gmail.com>
2023-12-12 10:14:53 -0500

    feat(Book): create createBookForm method

--- src/main/java/JPA_SHOP/JPA_SHOP/domain/item/Book.java
+++ src/main/java/JPA_SHOP/JPA_SHOP/domain/item/Book.java
@@ -13,4 +13,17 @@ public class Book extends Item {
 
   private String author;
   private String isbn;
+
+
+  public static Book createBookForm(Long id, String name, int price, int stockQuantity,
+      String author, String isbn) {
+    Book book = new Book();
+    book.setId(id);
+    book.setName(name);
+    book.setPrice(price);
+    book.setStockQuantity(stockQuantity);
+    book.setAuthor(author);
+    book.setIsbn(isbn);
+    return book;
+  }
 }
