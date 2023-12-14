00abae20e39f955d054e68c1febf97541ea00c36
Hyungchul You <teoyou881@gmail.com>
2023-12-14 16:59:09 -0500

    build(gradle): add     implementation 'com.fasterxml.jackson.datatype:jackson-datatype-hibernate5-jakarta' @bean hibernate5Module

--- src/main/java/JPA_SHOP/JPA_SHOP/JpaShopApplication.java
+++ src/main/java/JPA_SHOP/JPA_SHOP/JpaShopApplication.java
@@ -1,13 +1,20 @@
 package JPA_SHOP.JPA_SHOP;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaShopApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JpaShopApplication.class, args);
 
+  }
+
+  @Bean
+  Hibernate5JakartaModule hibernate5Module() {
+    return new Hibernate5JakartaModule();
+  }
 }
